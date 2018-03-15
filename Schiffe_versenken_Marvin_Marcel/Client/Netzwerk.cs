dab2d867ac5b582541296ae8f5487d75e95d76b4
MarcelSchulte <marcelschulte99@gmx.de>
2018-03-15 09:51:23 +0100

    2.

---
+++ Schiffe_versenken_Marvin_Marcel/Client/Netzwerk.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ClientMonitoring
+{
+    public static class Netzwerk
+    {
+        public static AsyncTcpClient client;
+        private static bool connect;
+
+        public static void Connect(IPv4Address.IPv4AddressTextBox txtip, TextBox txtport)
+        {
+            client = new AsyncTcpClient();
+            IPAddress address = IPAddress.Parse(txtip.Text);
+
+            
+            try
+            {
+                client.Connect(address, Convert.ToInt16(txtport.Text));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim verbinden mit dem Server.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void Disconnect()
+        {
+            client.Disconnect();
+        }
+
+        public static void IsConnectet(Label lblstatus)
+        {
+            if (client.IsConnected == true)
+            {
+                lblstatus.Invoke((MethodInvoker)(() => lblstatus.Text = "ist verbunden."));
+                lblstatus.Invoke((MethodInvoker)(() => lblstatus.ForeColor = Color.Green));
+                connect = true;
+            }
+            else if (connect == true)
+            {
+
+                lblstatus.Invoke((MethodInvoker)(() => lblstatus.Text = "ist nicht verbunden."));
+                lblstatus.Invoke((MethodInvoker)(() => lblstatus.ForeColor = Color.Red));
+                connect = false;
+                MessageBox.Show("Verbindung zum Server verloren.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        public static string DecryptString(string cipherText)       //Entschlüsselt die Daten
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+            // Erstellt die Sicherheitsschlüssel für die Verschlüsselung. Muss auf beiden Seiten gleich sein.
+            byte[] IV = { 0x51, 0xa2, 0x0b, 0xcc, 0x45, 0x06, 0x07, 0x08, 0x99, 0x1a, 0x1d, 0x12, 0x13, 0x1e, 0x15, 0x16 };
+            rijndaelCipher.Key = Convert.FromBase64String("ABCdfvdfknjvfdjknvfdknjfdkvfdmnj");
+            rijndaelCipher.IV = IV;
+
+            MemoryStream memoryStream = new MemoryStream(); // Erstellt ein Objekt das daten auf den Arbeitsspeicher schreiben kann.
+
+            ICryptoTransform rijndaelDecryptor = rijndaelCipher.CreateDecryptor();
+
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndaelDecryptor, CryptoStreamMode.Write);
+
+            string plainText = String.Empty;
+
+            try
+            {
+                byte[] cipherBytes = Convert.FromBase64String(cipherText);
+
+                cryptoStream.Write(cipherBytes, 0, cipherBytes.Length);
+
+                cryptoStream.FlushFinalBlock();
+
+                byte[] plainBytes = memoryStream.ToArray();
+
+                plainText = Encoding.ASCII.GetString(plainBytes, 0, plainBytes.Length);
+            }
+            finally
+            {
+                memoryStream.Close();
+                cryptoStream.Close();
+            }
+
+            return plainText;
+        }
+    }
+}
---
+++ Schiffe_versenken_Marvin_Marcel/Server/Netzwerk.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Security.Cryptography;
+
+namespace Server
+{
+    public static class Netzwerk
+    {
+        public static AsyncTcpServer server;
+        private static Label lblClients;
+       
+        public static void Starten(IPv4Address.IPv4AddressTextBox txtip, TextBox txtport, Label lblstatus, Label lblclients)
+        {
+            lblClients = lblclients;
+
+            IPAddress address = IPAddress.Parse(txtip.Text);
+            server = new AsyncTcpServer(address, Convert.ToInt16(txtport.Text));
+
+            server.ClientConnected += Server_ClientConnected;
+            server.ClientDisconnected += Server_ClientDisconnected;
+
+            try
+            {
+                server.Start();
+                lblstatus.Text = "Server gestartet";
+                lblstatus.ForeColor = Color.Green;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim starten vom Server.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        public static void Stoppen(Label lblstatus)
+        {
+            try
+            {
+                server.Stop();
+                lblstatus.Text = "Server ist aus.";
+                lblstatus.ForeColor = Color.Red;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Fehler beim stoppen vom Server.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void Server_ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            lblClients.Invoke((MethodInvoker)(() => lblClients.Text = $"{server.NumberOfConnectedClients}"));
+        }
+
+        private static void Server_ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            lblClients.Invoke((MethodInvoker)(() => lblClients.Text = $"{server.NumberOfConnectedClients}"));
+        }
+
+        public static void Senden_double(double data)
+        {
+            try
+            {
+                byte[] bytes = BitConverter.GetBytes(data);
+                server.SendPacket(bytes);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Fehler beim senden der Daten", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void Senden_string(string data)
+        {
+            try
+            {
+                byte[] bytes = Encoding.ASCII.GetBytes(EncryptString(data));
+                server.SendPacket(bytes);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Fehler beim senden der Daten.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static string EncryptString(string plainText)           //Verschlüsselung der Daten
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+            byte[] IV = { 0x51, 0xa2, 0x0b, 0xcc, 0x45, 0x06, 0x07, 0x08, 0x99, 0x1a, 0x1d, 0x12, 0x13, 0x1e, 0x15, 0x16 };
+            rijndaelCipher.Key = Convert.FromBase64String("ABCdfvdfknjvfdjknvfdknjfdkvfdmnj");
+            rijndaelCipher.IV = IV;
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            ICryptoTransform rijndaelEncryptor = rijndaelCipher.CreateEncryptor();
+
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndaelEncryptor, CryptoStreamMode.Write);
+
+            byte[] plainBytes = Encoding.ASCII.GetBytes(plainText);
+
+            cryptoStream.Write(plainBytes, 0, plainBytes.Length);
+
+            cryptoStream.FlushFinalBlock();
+
+            byte[] cipherBytes = memoryStream.ToArray();
+
+            memoryStream.Close();
+            cryptoStream.Close();
+
+            string cipherText = Convert.ToBase64String(cipherBytes, 0, cipherBytes.Length);
+
+            return cipherText;
+        }
+    }
+}
